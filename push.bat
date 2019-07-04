fa3e50374edfb4c0a199ce149a9becfb8f050d65
fpicerdas <fpibodoh@dropjar.com>
2019-07-04 13:05:19 +0700

    commit

--- push.bat
+++ push.bat
@@ -1,5 +1,12 @@
+:REDO
 git add .
 git commit -m "commit"
 git push origin master
-cmd /c start https://github.com/fpicerdas/amayya_tower/blob/master/ionic.project
+cmd /c start https://github.com/fpicerdas/amayya_tower
+:choice
+set /P c=Redo or Exit[r/x]?
+if /I "%c%" EQU "R" goto :REDO
+if /I "%c%" EQU "x" goto :EXIT
+goto :CHOICE
 pause
+:EXIT
